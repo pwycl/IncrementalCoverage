68c181ccdf72ff929a422caa58596bb61b12d091
pwycl <814821479@qq.com>
2020-12-21 10:42:13 +0800

    update example-fastjson-dev

--- src/example-fastjson-dev/test/FastjsonDriver.java
+++ src/example-fastjson-dev/test/FastjsonDriver.java
@@ -4,17 +4,13 @@ import com.alibaba.fastjson.JSON;
 import coverage.SubjectExecutor;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class FastjsonDriver extends SubjectExecutor {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         packagePrefix = "com.alibaba.fastjson";
-//        inputList = new ArrayList<>();
-//        inputList.add("{,,[}");
-//        inputList.add("[ true , false ]");
 
-        inputFileName = "src/example-fastjson-dev/.saveInput";
+        inputFileName = "src/example-fastjson-dev/stage-1.saveInput";
 
         new FastjsonDriver().wrapExecute();
     }
@@ -27,9 +23,5 @@ public class FastjsonDriver extends SubjectExecutor {
             JSON.toJSONString(JSON.parseObject(input, Object.class));
         }catch (Exception e){}
 
-//        json = "[ true , false ]";
-//        try {
-//            JSON.toJSONString(JSON.parseObject(json, Object.class));
-//        }catch (Exception e){}
     }
 }
