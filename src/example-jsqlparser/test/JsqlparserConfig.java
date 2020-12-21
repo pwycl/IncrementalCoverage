e58b04f25bca8d283a47ec7443c2c49401a7089d
pwycl <814821479@qq.com>
2020-12-21 12:44:05 +0800

    add example-jsqlparser

---
+++ src/example-jsqlparser/test/JsqlparserConfig.java
@@ -0,0 +1,8 @@
+package test;
+
+public class JsqlparserConfig {
+	public static boolean SYMB_FLAG=true;
+	
+	public static boolean TOKEN_SYMB=true;
+	
+}
---
+++ src/example-jsqlparser/test/TestJsqlparserDriver.java
@@ -0,0 +1,65 @@
+package test;
+
+import gov.nasa.jpf.jdart.Debug;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import gov.nasa.jpf.jdart.SymbolicString;
+import gov.nasa.jpf.symbc.Symbolic;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.Statements;
+
+
+
+
+public class TestJsqlparserDriver {
+
+	public static void main(String[] args) throws Exception{
+		start();
+	}
+	
+	public static void start() throws Exception {
+		String s = "SELECT * FROM Orders WHERE OrderDate='2008-12-26'";
+		
+//		File file=new File("src/example-jsqlparser/test.sql.in");
+//		FileInputStream in=new FileInputStream(file);
+//		int size=in.available();
+//		byte[] buffer=new byte[size];
+//		in.read(buffer);
+//		in.close();
+//		s=new String(buffer,"UTF-8");
+		
+//		if(JsqlparserConfig.SYMB_FLAG){
+//			char[] data;
+//			if(JsqlparserConfig.TOKEN_SYMB){
+//				Debug.setInput(s, "4");
+//				data=s.toCharArray();
+//				for(int i=0;i<data.length;++i){
+//					System.out.print(""+(int)data[i]+", ");
+//				}
+//				System.out.println();
+//			}else{
+//				data=s.toCharArray();
+//				for(int i=0;i<s.length();i++){
+//					if(((char)data[i])!=' '){
+//						data[i]=Debug.makeConcolicChar("sym_cnf_char"+i, ""+(int)data[i]);
+//						System.out.print(""+(int)data[i]+", ");
+//					}
+//				}
+//				System.out.println();
+//				s = new String(data);
+//			}
+//		}
+		s = SymbolicString.makeConcolicString(s);
+		System.out.println(s);
+		
+		Statements stmt = CCJSqlParserUtil.parseStatements(s);
+
+    }
+
+}
